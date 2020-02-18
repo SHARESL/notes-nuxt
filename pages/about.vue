5b113b8449a80b45d98073f83bcac457654d973c
Yosuke Inoue <ysk.inoue@sharesl.net>
2020-02-18 19:31:34 +0900

    # Aboutページ

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-about">
+    <MainVisual :title="'ABOUT'" :subtitle="'当サイトについて'" />
+  </div>
+  <!-- /.p-top -->
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+  import MainVisual from '~/components/MainVisual.vue'
+
+  export default {
+    components : {
+      MainVisual
+    }
+  }
+</script>
