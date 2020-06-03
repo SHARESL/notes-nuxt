cc90f2fcc51c106cdc3316e67b3001994b53aee2
Yosuke Inoue <ysk.inoue@sharesl.net>
2020-06-03 11:21:00 +0900

    # 修正・追加 - Contact form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,73 +3,73 @@
     <Title :title="title" :subtitle="subtitle"/>
     <div class="p-contact__inner">
       <client-only>
-        <validation-observer ref="observer" v-slot="{ invalid, validated }" tag="form" class="c-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
+        <validation-observer ref="observer" v-slot="{ invalid, validated }" tag="form" class="p-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
           <input type="hidden" name="form-name" value="contact">
-          <div class="c-contact__item">
-            <div class="c-contact__label">
+          <div class="p-contact__item">
+            <div class="p-contact__label">
               <label for="username">お名前</label>
             </div>
-            <!-- /.c-contact__label -->
-            <div class="c-contact__content">
+            <!-- /.p-contact__label -->
+            <div class="p-contact__content">
               <validation-provider v-slot="{ errors }" rules="required|max:100" name="お名前">
                 <input type="text" id="username" name="username" v-model="username" autocomplete="name">
                 <transition name="error">
-                  <p v-show="errors.length" class="c-contact__error">{{ errors[0] }}</p>
+                  <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
                 </transition>
               </validation-provider>
             </div>
-            <!-- /.c-contact__content -->
+            <!-- /.p-contact__content -->
           </div>
-          <!-- /.c-contact__item -->
-          <div class="c-contact__item">
-            <div class="c-contact__label">
+          <!-- /.p-contact__item -->
+          <div class="p-contact__item">
+            <div class="p-contact__label">
               <label for="useremail">メールアドレス</label>
             </div>
-            <!-- /.c-contact__label -->
-            <div class="c-contact__content">
+            <!-- /.p-contact__label -->
+            <div class="p-contact__content">
               <validation-provider v-slot="{ errors }" rules="required|email|max:256" name="メールアドレス">
                 <input type="text" id="useremail" name="useremail" v-model="useremail" autocomplete="email" required>
                 <transition name="error">
-                  <p v-show="errors.length" class="c-contact__error">{{ errors[0] }}</p>
+                  <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
                 </transition>
               </validation-provider>
             </div>
-            <!-- /.c-contact__content -->
+            <!-- /.p-contact__content -->
           </div>
-          <!-- /.c-contact__item -->
-          <div class="c-contact__item">
-            <div class="c-contact__label">
+          <!-- /.p-contact__item -->
+          <div class="p-contact__item">
+            <div class="p-contact__label">
               <label for="message">ご質問・ご要望など</label>
             </div>
-            <!-- /.c-contact__label -->
-            <div class="c-contact__content">
+            <!-- /.p-contact__label -->
+            <div class="p-contact__content">
               <validation-provider v-slot="{ errors }" rules="required|max:1000" name="ご質問・ご要望など">
                 <textarea id="message" name="message" v-model="message" required></textarea>
                 <transition name="error">
-                  <p v-show="errors.length" class="c-contact__error">{{ errors[0] }}</p>
+                  <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
                 </transition>
               </validation-provider>
             </div>
-            <!-- /.c-contact__content -->
+            <!-- /.p-contact__content -->
           </div>
-          <!-- /.c-contact__item -->
-          <div class="c-contact__item" v-show="false">
-            <div class="c-contact__label">
+          <!-- /.p-contact__item -->
+          <div class="p-contact__item" v-show="false">
+            <div class="p-contact__label">
               <label for="message">スパムでない場合は空欄</label>
             </div>
-            <!-- /.c-contact__label -->
-            <div class="c-contact__content">
+            <!-- /.p-contact__label -->
+            <div class="p-contact__content">
               <input type="text" name="bot-field" v-model="botField"/>
             </div>
-            <!-- /.c-contact__content -->
+            <!-- /.p-contact__content -->
           </div>
-          <!-- /.c-contact__item -->
-          <div class="c-contact__item c-contact__item--submit">
-            <button class="c-contact__btn c-btn" type="submit" :disabled="invalid || !validated">送信</button>
+          <!-- /.p-contact__item -->
+          <div class="p-contact__submit c-btn">
+            <button class="p-contact__btn" type="submit" :disabled="invalid || !validated">送信</button>
           </div>
-          <!-- /.c-contact__item -->
+          <!-- /.p-contact__submit -->
         </validation-observer>
-        <!-- /.c-contact__form -->
+        <!-- /.p-contact__form -->
       </client-only>
     </div>
     <!-- /.p-contact__inner -->
